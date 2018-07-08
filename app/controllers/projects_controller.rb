91cf7b1dd4ad5ea345b920c74659e25b08dff23c
Y9uhei9031 <youhei.ueno9031@gmail.com>
2018-07-08 23:14:32 +0900

    プロジェクト登録フォーム作成しました。

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,6 +3,14 @@ class ProjectsController < ApplicationController
     @projects = Project.all.order(created_at: 'desc')
   end
 
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def new
+    @project = Project.new
+  end
+
   def create
   end
 
