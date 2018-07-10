57f644fdd3d7131fdcb905dcc36de1d2a01d8b42
Y9uhei9031 <youhei.ueno9031@gmail.com>
2018-07-10 22:56:37 +0900

    タスクの詳細画面でコメントできます。

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,20 @@
 class CommentsController < ApplicationController
+
+  def create
+    @task = Task.find(params[:task_id])
+    @task.comments.create(comment_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:task_id])
+    @comment = @task.comments.find(params[:id])
+    @comment.destroy
+    redirect_to task_path(@task)
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
 end
