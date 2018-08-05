a44a153745a69d7746c18edd453436dd4e1d967e
Y9uhei9031 <youhei.ueno9031@gmail.com>
2018-08-05 15:37:16 +0900

    ログイン時と非ログイン時の/pathを設定しました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   resources :posts, only: [:create]
   devise_for :users
+  devise_scope :user do
+    authenticated :user do
+      root :to => "pages#show", as: :authenticated_root
+    end
+    unauthenticated :user do
+      root :to => 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'pages/show'
   resources :projects do
@@ -11,4 +19,5 @@ Rails.application.routes.draw do
   end
 
   post 'projects/:project_id/tasks/:id/toggle' => 'tasks#toggle'
+
 end
