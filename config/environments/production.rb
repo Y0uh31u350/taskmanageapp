0bf35d7ef64bd64f9421369ff16dad0630c16b02
Y9uhei9031 <youhei.ueno9031@gmail.com>
2018-08-07 22:55:03 +0900

    production.rb修正しました。

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,17 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.action_mailer.default_url_options = { host: "task-manageapp.herokuapp.com" }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    user_name: "youhei.test913@gmail.com",
+    password: "@bc123456",
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
